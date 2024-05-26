72a2dbcc93bba7dafa1fdcfc289061578893f181
Goga211 <antipingv2003@gmail.com>
2024-05-26 23:12:50 +0800

    style + theme

--- js/main.js
+++ js/main.js
@@ -5,22 +5,45 @@ document.addEventListener('DOMContentLoaded', function() {
     const javaDiv = document.querySelector('div.Java');
     const cppDiv = document.querySelector('div.C');
     const pythonDiv = document.querySelector('div.Python');
+    let theme = localStorage.getItem("theme") ?? 'blackAndWhite';
 
     javaBtn.addEventListener('click', function() {
+        document.querySelectorAll('.language button').forEach((item) => {
+            item.classList.remove('selected');
+        });
+        this.classList.add('selected');
         javaDiv.style.display = 'flex';
         cppDiv.style.display = 'none';
         pythonDiv.style.display = 'none';
     });
 
     cppBtn.addEventListener('click', function() {
+        document.querySelectorAll('.language button').forEach((item) => {
+            item.classList.remove('selected');
+        });
+        this.classList.add('selected');
         javaDiv.style.display = 'none';
         cppDiv.style.display = 'flex';
         pythonDiv.style.display = 'none';
     });
 
     pythonBtn.addEventListener('click', function() {
+        document.querySelectorAll('.language button').forEach((item) => {
+            item.classList.remove('selected');
+        });
+        this.classList.add('selected');
         javaDiv.style.display = 'none';
         cppDiv.style.display = 'none';
         pythonDiv.style.display = 'flex';
+    });
+
+    document.getElementById('change-theme').addEventListener('click', () => {
+        if (theme === 'blackAndWhite') {
+            document.classList.add('native-dark');
+            theme = 'nativeDark';
+        } else {
+            document.classList.remove('native-dark');
+            theme = 'blackAndWhite';
+        }
     });
 });
---
+++ js/theme.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.getItem('theme') == null)
+        localStorage.setItem('theme', 'blackAndWhite')
+
+    let theme = localStorage.getItem("theme");
+
+    if (theme !== 'blackAndWhite')
+        document.querySelector("html").classList.add('native-dark');
+
+    document.getElementById('change-theme').addEventListener('click', () => {
+        if (theme === 'blackAndWhite') {
+            document.querySelector("html").classList.add('native-dark');
+            theme = 'nativeDark';
+            localStorage.setItem("theme", 'nativeDark');
+        } else {
+            document.querySelector("html").classList.remove('native-dark');
+            theme = 'blackAndWhite';
+            localStorage.setItem("theme", 'blackAndWhite');
+        }
+    });
+});
